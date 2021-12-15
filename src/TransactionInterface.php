eab76924fa687cb59423f6a6a2faf94db058ad78
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-12-16 03:25:36 +0900

    Revert TransactionInterface.php

--- src/TransactionInterface.php
+++ src/TransactionInterface.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace Pj8\SentryModule;
 
-use Sentry\Tracing\Transaction as SentryTransaction;
-
 interface TransactionInterface extends StartChildInterface
 {
-    public function getTransaction(): SentryTransaction;
-
-    public function setTransaction(SentryTransaction $transaction): void;
 }
