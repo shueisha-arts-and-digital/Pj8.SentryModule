25f4d1c382e2b432e263b3425a837c544b1c4f52
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-12-16 03:23:28 +0900

    Revert Monitorable attribute

--- src/Annotation/Monitorable.php
+++ src/Annotation/Monitorable.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Pj8\SentryModule\Annotation;
 
+use Attribute;
+
 /**
  * @Annotation
  * @Target("METHOD")
  */
+#[Attribute(Attribute::TARGET_METHOD)]
 final class Monitorable
 {
 }
